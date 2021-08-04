0104ccb10cd014882d5eceb532c0b90071599740
Zamran J <family_zj@yahoo.com>
2021-08-04 18:54:35 -0500

    finished with bonus stuff

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response){
+        response.json().then(function(json) {
+            // console.log(json);
+            const container = document.getElementById("container")
+            json.sort((a, b) =>(a.hoursInSpace > b.hoursInSpace)? 1 : -1);
+            for(let i=0; i<json.length; i++){
+                container.innerHTML += 
+                `
+                <div class="astronaut">
+                <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                   <ul>
+                      <li>Hours in space: ${json[i].hoursInSpace}</li>
+                      <li id="${json[i].id}">Active: ${json[i].active}</li>
+                      <li>Skills: ${json[i].skills.join(", ")}</li>
+                   </ul>
+                </div>
+                <img class="avatar" src=${json[i].picture}>
+                </div>
+             `
+            if(json[i].active === true){
+                    const setColor = document.getElementById(String(json[i].id));
+                    setColor.style.color = "green";
+                };
+            }
+       });
+    });
+});
